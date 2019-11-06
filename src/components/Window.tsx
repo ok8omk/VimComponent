f57ebb51a7f065bc6bb13e3adbba98f03231fb68
Tomoki Oke(LITALICO) <tomoki.oke@litalico.co.jp>
2019-11-06 19:19:31 +0900

    add window

---
+++ src/components/Window.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import styled from "@emotion/styled";
+
+const Container = styled.h1`
+  width: 500px;
+  height: 500px;
+  border: solid 1px black;
+`;
+
+export const Window = () => <Container />;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
+import { Window } from "./components/Window";
 
-ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
-);
+ReactDOM.render(<Window />, document.getElementById("example"));
