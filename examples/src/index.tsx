f1a4d5bd3742b89fc657b2d8c175c0a61b67840b
Tomoki Oke(LITALICO) <tomoki.oke@litalico.co.jp>
2019-12-01 11:09:47 +0900

    :heavy_plus_sign:add ghpages

---
+++ examples/src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Vinput from "../../src";
+
+ReactDOM.render(<Vinput />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,17 @@
-var path = require("path");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: path.join(__dirname, "examples/src/index.html"),
+  filename: "./index.html"
+});
 
 module.exports = {
-  mode: "development",
-
-  // Enable sourcemaps for debugging webpack's output.
   devtool: "source-map",
-
-  entry: "./src/index.tsx",
-
+  entry: path.join(__dirname, "examples/src/index.tsx"),
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: [".ts", ".tsx", ".js"]
   },
-
   module: {
     rules: [
       {
@@ -24,7 +23,6 @@ module.exports = {
           }
         ]
       },
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       {
         enforce: "pre",
         test: /\.js$/,
@@ -32,17 +30,14 @@ module.exports = {
       }
     ]
   },
+  plugins: [htmlWebpackPlugin],
   devServer: {
-    contentBase: path.join(__dirname),
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 9000
   },
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    react: "React",
-    "react-dom": "ReactDOM"
+  output: {
+    path: path.join(__dirname, "examples/dist"),
+    filename: "bundle.js"
   }
 };
